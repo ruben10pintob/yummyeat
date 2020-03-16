321a324e4edc36c7f055cd681fe70a61d98dc4d7
sergio140299 <sergio140299@gmail.com>
2020-03-16 20:35:17 +0100

    Cambio en la bdd de productos. Nueva tabla producto-restaurante con los productos del Dominos

--- bdd/yummyeat.sql
+++ bdd/yummyeat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2020 a las 17:01:36
--- Versión del servidor: 10.1.35-MariaDB
--- Versión de PHP: 7.2.9
+-- Tiempo de generación: 16-03-2020 a las 20:27:08
+-- Versión del servidor: 10.4.8-MariaDB
+-- Versión de PHP: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -111,11 +111,53 @@ CREATE TABLE `pedido` (
 
 CREATE TABLE `producto` (
   `ID_PRODUCTO` int(11) NOT NULL,
-  `ID_RESTAURANTE` char(4) NOT NULL,
   `NOMBRE_PRODUCTO` varchar(40) NOT NULL,
   `DESCRIPCION_PRODUCTO` varchar(150) NOT NULL,
   `PRECIO_PRODUCTO` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`ID_PRODUCTO`, `NOMBRE_PRODUCTO`, `DESCRIPCION_PRODUCTO`, `PRECIO_PRODUCTO`) VALUES
+(1, 'New York MeatBalls', 'Salsa de tomate, queso 100% mozzarella, pepperoni, meatballs, salsa BBQ por encima y queso cheddar en los bordes.', '12.90'),
+(2, 'Oklahoma Bacon Crispy', 'Salsa BBQ original, queso 100% mozzarella, pepperoni, york, bacon cripy y queso cheddar en el borde.', '12.90'),
+(3, 'Texas BBQ Crispy', 'Salsa BBQ Texas, queso 100% mozzarella, bacon crispy, bacon, pollo a la parrilla, carne de vacuno, queso cheddar en el borde y salsa.', '12.90'),
+(4, 'Cremozza al estilo Bourbon', 'Base de crema fresca, queso 100% mozzarella, bacon, pollo a la parrilla, cebolla y salsa Bourbon', '11.90'),
+(5, 'Cremozza BBQ', 'Base de crema fresca, queso 100% mozzarella, bacon, pollo a la parrila, cebolla, salsa barbacoa.', '11.90'),
+(6, 'Cabramelizada', 'Crema fresca, queso 100% mozzarella, queso de cabra, cebolla caramelizada.', '11.90'),
+(7, 'Cheesix', 'Crema fresca, queso 100% mozzarella, cheddar, emmental, parmesano, gorgonzola, queso de cabra.', '11.90'),
+(8, 'Pecado Carnal', 'Salsa de tomate, extra de queso 100% mozzarella, carne de vacuno, bacon, pepperoni, york.', '10.90'),
+(9, 'Carbonara', 'Crema fresca, queso 100% mozzarella, bacon, champiñon y cebolla.', '10.90'),
+(10, 'Extravaganzza', 'Sala de tomate, mozzarella, carne de vacuno, pepperoni, York, bacon, cebolla, pimiento verde, champiñon y aceitunas negras.', '10.90'),
+(11, 'Barbacoa', 'Salsa barbacoa, queso 100% mozzarella, carne de vacuno, cebolla, bacon, maiz', '10.90'),
+(12, 'Hawaiana Plus', 'Salsa de tomate, extra de queso 100% mozzarella, doble de York y piña.', '10.90'),
+(13, 'Pollo a la Parrilla', 'Salsa de tomate, queso 100% mozzarella, doble de pollo a la parrilla, cebolla, champiñon y maíz.', '10.90'),
+(14, 'Cuatro  Quesos', 'Salsa de tomate, queso 100% mozzarella, cheddar, emmental y gorgonzola.', '10.90'),
+(15, 'Tony Pepperoni', 'Salsa de tomate, extra de queso 100% mozzarella y doble de pepperoni.', '10.90'),
+(16, 'Patanegra', 'Salsa de tomate, queso 100% mozzarella, jamón serrano, aceite de oliva, aceitunas negras, tomate natural y orégano.', '10.90'),
+(17, 'Campiña', 'Salsa de tomate, extra de queso 100% mozzarella, champiñón, pimiento verde, cebolla, aceitunas negras y tomate natural.', '10.90'),
+(19, 'Cheese &  Bacon Fries', 'Nuestras patatas con bacon crispy y deliciosa salsa cheddar.', '7.90'),
+(20, 'Patatas Grill', 'Crujientes patatas horneadas acompañadas de una salsa a elegir.', '7.90'),
+(21, 'Combo de pollo', 'Strippers, Kickers y Alitas (17 uds).', '9.90'),
+(22, 'Combo de alitas', 'Alitas en salsa Bourbon, Mostaza y miel y Barbacoa (18 uds).', '9.90'),
+(23, 'Alitas de pollo', 'Sabrosas alitas de pollo marinadas (6uds).', '6.90'),
+(24, 'Alitas de polllo con salsa', 'Sabrosas alitas de pollo marinadas con salsa.', '7.90'),
+(25, 'Kickers', 'Pequeñas pechugas de pollo empanadas, con rebozado picante (6uds).', '6.90'),
+(26, 'Strippers', 'Tiras de pollo empanadas (5uds).', '6.90'),
+(27, 'Camembits', 'Deliciosos bocados de queso que se funden en tu boca.', '6.90'),
+(28, 'Pan de ajo con queso', 'Crujiente pan de ajo tostado con queso 100% mozzarella fundido (4 o 6 uds).', '0.00'),
+(29, 'Brownkie', 'Una deliciosa unión de cookies y brownie fundido.', '4.95'),
+(30, 'Chococheese', 'Cremoso volcán de queso con una esponjosa base de brownie de chocolate.', '4.95'),
+(35, 'Cookies', 'Deliciosas galletas con pepitas de chocolate.', '3.95'),
+(36, 'Vulcano de chocolate', 'Delicioso bizcocho de chocolate relleno de chocolate fundido.', '3.95'),
+(37, 'Pepsi', 'Pepsi clásica lata 33cl.', '1.50'),
+(38, 'Pepsi Max', 'Pepsi Maaxlata 33cl.', '1.50'),
+(39, 'Red Bull', 'Red Bull lata 25cl.', '2.30'),
+(40, 'Kas Naranja', 'Kas Naranja lata 33cl.', '1.50'),
+(43, 'Kas Limón', 'Kas Limón lata 33cl.', '1.50'),
+(44, 'Agua', 'Agua sin gas 50cl.', '1.10');
 
 -- --------------------------------------------------------
 
@@ -127,6 +169,198 @@ CREATE TABLE `productos_menu` (
   `ID_MENU` int(11) NOT NULL,
   `ID_PRODUCTO` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_restaurante`
+--
+
+CREATE TABLE `producto_restaurante` (
+  `ID_PRODUCTO` int(4) NOT NULL,
+  `ID_RESTAURANTE` char(4) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto_restaurante`
+--
+
+INSERT INTO `producto_restaurante` (`ID_PRODUCTO`, `ID_RESTAURANTE`) VALUES
+(1, 'DPAL'),
+(2, 'DPAL'),
+(3, 'DPAL'),
+(4, 'DPAL'),
+(5, 'DPAL'),
+(6, 'DPAL'),
+(7, 'DPAL'),
+(8, 'DPAL'),
+(9, 'DPAL'),
+(10, 'DPAL'),
+(11, 'DPAL'),
+(12, 'DPAL'),
+(13, 'DPAL'),
+(14, 'DPAL'),
+(15, 'DPAL'),
+(16, 'DPAL'),
+(17, 'DPAL'),
+(19, 'DPAL'),
+(20, 'DPAL'),
+(21, 'DPAL'),
+(22, 'DPAL'),
+(23, 'DPAL'),
+(24, 'DPAL'),
+(25, 'DPAL'),
+(26, 'DPAL'),
+(27, 'DPAL'),
+(28, 'DPAL'),
+(29, 'DPAL'),
+(30, 'DPAL'),
+(37, 'DPAL'),
+(38, 'DPAL'),
+(39, 'DPAL'),
+(40, 'DPAL'),
+(35, 'DPAL'),
+(36, 'DPAL'),
+(1, 'DPFU'),
+(2, 'DPFU'),
+(3, 'DPFU'),
+(4, 'DPFU'),
+(5, 'DPFU'),
+(6, 'DPFU'),
+(7, 'DPFU'),
+(8, 'DPFU'),
+(9, 'DPFU'),
+(10, 'DPFU'),
+(11, 'DPFU'),
+(12, 'DPFU'),
+(13, 'DPFU'),
+(14, 'DPFU'),
+(15, 'DPFU'),
+(16, 'DPFU'),
+(17, 'DPFU'),
+(19, 'DPFU'),
+(20, 'DPFU'),
+(21, 'DPFU'),
+(22, 'DPFU'),
+(23, 'DPFU'),
+(24, 'DPFU'),
+(25, 'DPFU'),
+(26, 'DPFU'),
+(27, 'DPFU'),
+(28, 'DPFU'),
+(29, 'DPFU'),
+(30, 'DPFU'),
+(37, 'DPFU'),
+(38, 'DPFU'),
+(39, 'DPFU'),
+(40, 'DPFU'),
+(35, 'DPFU'),
+(36, 'DPFU'),
+(1, 'DPMA'),
+(2, 'DPMA'),
+(3, 'DPMA'),
+(4, 'DPMA'),
+(5, 'DPMA'),
+(6, 'DPMA'),
+(7, 'DPMA'),
+(8, 'DPMA'),
+(9, 'DPMA'),
+(10, 'DPMA'),
+(11, 'DPMA'),
+(12, 'DPMA'),
+(13, 'DPMA'),
+(14, 'DPMA'),
+(15, 'DPMA'),
+(16, 'DPMA'),
+(17, 'DPMA'),
+(19, 'DPMA'),
+(20, 'DPMA'),
+(21, 'DPMA'),
+(22, 'DPMA'),
+(23, 'DPMA'),
+(24, 'DPMA'),
+(25, 'DPMA'),
+(26, 'DPMA'),
+(27, 'DPMA'),
+(28, 'DPMA'),
+(29, 'DPMA'),
+(30, 'DPMA'),
+(37, 'DPMA'),
+(38, 'DPMA'),
+(39, 'DPMA'),
+(40, 'DPMA'),
+(35, 'DPMA'),
+(36, 'DPMA'),
+(1, 'DPPI'),
+(2, 'DPPI'),
+(3, 'DPPI'),
+(4, 'DPPI'),
+(5, 'DPPI'),
+(6, 'DPPI'),
+(7, 'DPPI'),
+(8, 'DPPI'),
+(9, 'DPPI'),
+(10, 'DPPI'),
+(11, 'DPPI'),
+(12, 'DPPI'),
+(13, 'DPPI'),
+(14, 'DPPI'),
+(15, 'DPPI'),
+(16, 'DPPI'),
+(17, 'DPPI'),
+(19, 'DPPI'),
+(20, 'DPPI'),
+(21, 'DPPI'),
+(22, 'DPPI'),
+(23, 'DPPI'),
+(24, 'DPPI'),
+(25, 'DPPI'),
+(26, 'DPPI'),
+(27, 'DPPI'),
+(28, 'DPPI'),
+(29, 'DPPI'),
+(30, 'DPPI'),
+(37, 'DPPI'),
+(38, 'DPPI'),
+(39, 'DPPI'),
+(40, 'DPPI'),
+(35, 'DPPI'),
+(36, 'DPPI'),
+(1, 'DPVA'),
+(2, 'DPVA'),
+(3, 'DPVA'),
+(4, 'DPVA'),
+(5, 'DPVA'),
+(6, 'DPVA'),
+(7, 'DPVA'),
+(8, 'DPVA'),
+(9, 'DPVA'),
+(10, 'DPVA'),
+(11, 'DPVA'),
+(12, 'DPVA'),
+(13, 'DPVA'),
+(14, 'DPVA'),
+(15, 'DPVA'),
+(16, 'DPVA'),
+(17, 'DPVA'),
+(19, 'DPVA'),
+(20, 'DPVA'),
+(21, 'DPVA'),
+(22, 'DPVA'),
+(23, 'DPVA'),
+(24, 'DPVA'),
+(25, 'DPVA'),
+(26, 'DPVA'),
+(27, 'DPVA'),
+(28, 'DPVA'),
+(29, 'DPVA'),
+(30, 'DPVA'),
+(37, 'DPVA'),
+(38, 'DPVA'),
+(39, 'DPVA'),
+(40, 'DPVA'),
+(35, 'DPVA'),
+(36, 'DPVA');
 
 -- --------------------------------------------------------
 
@@ -251,8 +485,7 @@ ALTER TABLE `pedido`
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD PRIMARY KEY (`ID_PRODUCTO`),
-  ADD KEY `ID_RESTAURANTE` (`ID_RESTAURANTE`);
+  ADD PRIMARY KEY (`ID_PRODUCTO`);
 
 --
 -- Indices de la tabla `productos_menu`
@@ -260,6 +493,13 @@ ALTER TABLE `producto`
 ALTER TABLE `productos_menu`
   ADD PRIMARY KEY (`ID_MENU`,`ID_PRODUCTO`),
   ADD KEY `ID_PRODUCTO` (`ID_PRODUCTO`);
+
+--
+-- Indices de la tabla `producto_restaurante`
+--
+ALTER TABLE `producto_restaurante`
+  ADD KEY `ID_PRODUCTO` (`ID_PRODUCTO`),
+  ADD KEY `ID_RESTAURANTE` (`ID_RESTAURANTE`);
 
 --
 -- Indices de la tabla `repartidor`
@@ -300,7 +540,7 @@ ALTER TABLE `pedido`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT de la tabla `repartidor`
@@ -341,17 +581,18 @@ ALTER TABLE `pedido`
   ADD CONSTRAINT `pedido_ibfk_3` FOREIGN KEY (`ID_REPARTIDOR`) REFERENCES `repartidor` (`ID_REPARTIDOR`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`ID_RESTAURANTE`) REFERENCES `restaurante` (`ID_RESTAURANTE`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `productos_menu`
 --
 ALTER TABLE `productos_menu`
   ADD CONSTRAINT `productos_menu_ibfk_1` FOREIGN KEY (`ID_PRODUCTO`) REFERENCES `producto` (`ID_PRODUCTO`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `productos_menu_ibfk_2` FOREIGN KEY (`ID_MENU`) REFERENCES `menu` (`ID_MENU`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto_restaurante`
+--
+ALTER TABLE `producto_restaurante`
+  ADD CONSTRAINT `producto_restaurante_ibfk_1` FOREIGN KEY (`ID_PRODUCTO`) REFERENCES `producto` (`ID_PRODUCTO`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `producto_restaurante_ibfk_2` FOREIGN KEY (`ID_RESTAURANTE`) REFERENCES `restaurante` (`ID_RESTAURANTE`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
