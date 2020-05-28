6bbf15cdfc2d0a5db1094e7a74505d693054b94c
ruben <ruben10pintob@gmail.com>
2020-05-28 14:33:18 +0200

    Cambios en inicio de sesion registrarse y buscar por ubicacion

--- sesion/inicio-sesion.php
+++ sesion/inicio-sesion.php
@@ -9,6 +9,13 @@ if(haySesionIniciada()) redireccionar("../cliente/inicio.php");
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="../bootstrap/bootstrap-4.5.0-dist/js/bootstrap.js"></script>
+    <script src="../bootstrap/bootstrap-4.5.0-dist/js/popper.min.js"></script>
+    <script src="../bootstrap/bootstrap-4.5.0-dist/js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="../bootstrap/bootstrap-4.5.0-dist/css/bootstrap.css">
 </head>
 
 <body>
